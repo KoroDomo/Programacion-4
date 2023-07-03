814f65a10c8450754d497d935e5195e8e5793619
dioris.adp@gmail.com <dioris.adp@gmail.com>
2023-07-02 23:31:10 -0400

    Practica P02-INF-5190 Finalizada y enviada

---
+++ P02_INF_5190/Persona.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_INF_5190
+{
+    internal class Persona
+    {
+        /* Campos o Atributos.  Tienen modificador de acceso private debdio a encapsulamiento de datos. Accedemos
+         * a estos a traves de las propiedades (getters y setters) */
+        private string nombre;
+        private string apellidos;
+        private string sexo;
+        private byte edad;
+        private double cedula;
+
+        // Metodo Constructor, inicializamos los atributos creados
+        public Persona()
+        {
+            nombre = "";
+            apellidos = "";
+            sexo = "";
+            edad = 0;
+            cedula = 0;
+        }
+
+        //Getters y Setters
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+
+        public string Sexo
+        {
+            get { return sexo; }
+            //utilizamos el metodo Setter para validar el sexo ingresado, si es incorrecto le asigna el valor "Indefinido" 
+            set {
+                if (String.Equals(value, "M") || String.Equals(value, "F"))
+                    sexo = value;
+                else
+                {
+                    Console.WriteLine("El sexo es incorrecto");
+                    sexo = "Indefinido";
+                }                                     
+            }
+        }
+
+        public byte Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public double Cedula
+        {
+            get { return cedula; }
+            set { cedula = value; }
+        }
+
+        // Overload del metodo ToString para imprimir contenido de los objetos personas
+        public override string ToString()
+        {
+            //esSexoCorrecto();
+            return "Nombre: " + Nombre + " | Apellidos: " + Apellidos + " | Sexo: " + Sexo + " | Edad: " + Edad + " | Es Mayor de edad?: " + esMayor() + " | Cedula: " + Cedula;
+        }
+
+        // Metodo para identificar si la persona es mayor de edad
+        public bool esMayor() => Edad >= 18 ? true : false;
+
+        //public void esSexoCorrecto()
+        //{
+        //    if String.Equals("M")
+        //        Console.WriteLine("El sexo ingresado \"{0}\" es incorrecto.", Sexo);
+        //}
+    }
+}
---
+++ P02_INF_5190/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using P02_INF_5190;
+using System.Collections;
+
+//Declaracion de Variables
+string nombre, apellidos, sexo, seguir;
+byte edad;
+double cedula;
+
+do
+{
+    //Arraylisy donde se guardaran las personas
+    ArrayList personas = new ArrayList();
+    
+    //Objetos de personas
+    Persona persona1 = new Persona();
+    Persona persona2 = new Persona();
+    Persona persona3 = new Persona();
+
+
+    Console.WriteLine("***********************INGRESE DATOS DE PERSONAS*******************\n");
+    /* Utilizamos el metodo capturador() parar crear los objetos con los datos ingresados por teclados a traves de las propiedades (getters/setters)
+     * Luego, los agregamos al ArrayList */
+    personas.Add(capturador(persona1));
+    personas.Add(capturador(persona2));
+    personas.Add(capturador(persona3));
+
+    // Bucle para recorrer cada persona denetro del arraylist e imprimir sus datos haciendo uso del toString()
+    foreach (Persona persona in personas)
+    {
+        Console.WriteLine(persona.ToString());
+    }
+
+    //Confirmamos si deseamos agregar nuevas personas
+    Console.WriteLine("\nDesea agregar 3 personas mas? (Si \\ No)");
+    seguir = Console.ReadLine().ToLower();
+} while (String.Equals(seguir, "si"));
+
+
+// Metodo para capturar los datos de las personas. Acepta el objeto Persona instanciado y lo retorna con los datos capturados
+Persona capturador(Persona persona)
+{
+    Console.Write("Ingrese Nombre: ");
+    persona.Nombre = Console.ReadLine();
+
+    Console.Write("Ingrese Apellidos: ");
+    persona.Apellidos = Console.ReadLine();
+
+    Console.Write("Ingrese Sexo (M\\F): ");
+    persona.Sexo = Console.ReadLine().ToUpper();
+
+    Console.Write("Ingrese Edad: ");
+    persona.Edad = Convert.ToByte(Console.ReadLine());
+
+    Console.Write("Ingrese Cedula: ");
+    persona.Cedula = Convert.ToDouble(Console.ReadLine());
+
+    Console.WriteLine();
+
+    return persona;
+}
