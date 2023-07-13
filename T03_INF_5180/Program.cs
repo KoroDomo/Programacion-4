a74fa4fb876bfe013f420ad3ff91a7ee1a43cf99
dioris.adp@gmail.com <dioris.adp@gmail.com>
2023-07-12 23:05:24 -0400

    Agregados todos los metodos

--- T03_INF_5180/Program.cs
+++ T03_INF_5180/Program.cs
@@ -11,7 +11,7 @@ List<Estudiante> lista = new List<Estudiante>()
     new Estudiante(123456789, "Oflo", "Santos", 36, 100),
     new Estudiante(999999999, "Melvin", "Quinones", 35, 95),
     new Estudiante(484848489, "Fulano", "Detal", 25, 80),
-    new Estudiante(565656567, "Sutaneja", "Perez", 24, 88),
+    new Estudiante(565656567, "Sutaneja", "Perez", 14, 88),
 };
 //List<Estudiante> lista = new List<Estudiante>
 //{
@@ -36,6 +36,24 @@ do
         case 3:
             Eliminar(lista);
             break;
+        case 4:
+            Buscar(lista);
+            break;
+        case 5:
+            BuscarMatricula(lista);
+            break;
+        case 6:
+            DesplegarSetentaNoventa(lista);
+            break;
+        case 7:
+            DesplegarDoceQuince(lista);
+            break;
+        case 8:
+            DesplegarDescendente(lista);
+            break;
+        case 9:
+            DesplegarEstudiantes(lista);
+            break;
     }
 
     
@@ -59,7 +77,7 @@ void Agregar()
 
     lista.Add(new Estudiante(matricula, nombre, apellidos, edad, promedio));
 
-    Escribir("\nEstudiante agregado. Presione cualquier tecla para regresar");
+    Escribir("\nEstudiante agregado. Presione cualquier tecla para regresar...");
     Leer();
     Console.Clear();
 }
@@ -91,26 +109,150 @@ void Editar(List<Estudiante> alumno)
     else
         Escribir("\n*******ERROR: La Opcion ingresada es incorrecta.");
 
-    Escribir("\nEstudiante editado. Presione cualquier tecla para regresar");
+    Escribir("\nEstudiante editado. Presione cualquier tecla para regresar...");
     Leer();
     Console.Clear();
 
 }
 
-// 3- Eliminar estudiante
+// 3- Eliminar Estudiante
 
 void Eliminar(List<Estudiante> alumno) {
-    int x, z, i = 0;
+    int z, i = 0;
     //Mostrando Nombre de Estudiantes guardados usando LinQ y metodo ToString para desplegar todos los datos
     
     alumno.ForEach(alu => Console.WriteLine(++i + ": " + alu.ToString()));
 
     Escribir("\nIngrese la Matricula del Estudiante a eliminar: ");
     z = LeerEntero();
-    //Eliminando Estudiantes cuya matricula sea igual a la ingresada
+    //Eliminando Estudiantes cuya matricula sea igual a la ingresada usando LinQ
     alumno.RemoveAll(mat => mat.Matricula == z);
 
-    Escribir("\nEstudiante eliminado. Presione cualquier tecla para regresar");
+    Escribir("\nEstudiante eliminado. Presione cualquier tecla para regresar...");
+    Leer();
+    Console.Clear();
+}
+
+// 4- Buscar Estudiante (Por Nombre)
+void Buscar(List<Estudiante> alumno)
+{
+    string nombre;
+ 
+    Escribir("\nIngrese el Nombre del Estudiante a Buscar: ");
+    nombre = Leer();
+    // Buscando nombre dentro de lista Alumno usando LinQ. Si existe, retorna booleano
+    bool existe = alumno.Any(nom => nom.Nombre ==  nombre);
+
+    if (existe)
+    {
+        // Se instancia una variable con el nombre encontrado y se imprimen sus datos
+        Estudiante encontrado = alumno.Find(e => e.Nombre == nombre);
+        Escribir("ESTUDIANTE ENCONTRADO: ");
+        Escribir(encontrado.ToString());
+    }
+    else
+        Escribir("ESTUDIANTE NO ENCONTRADO...");
+
+    Escribir("\nPresione cualquier tecla para regresar...");
+    Leer();
+    Console.Clear();
+}
+
+// 5- Buscar Estudiante (Por Matricula)
+void BuscarMatricula(List<Estudiante> alumno)
+{
+    int z;
+
+    Escribir("\nIngrese la Matricula del Estudiante a Buscar: ");
+    z = LeerEntero();
+    // Buscando matricula dentro de lista Alumno usando LinQ. Si existe, retorna booleano
+    bool existe = alumno.Any(mat => mat.Matricula == z);
+
+    if (existe)
+    {
+        // Se instancia una variable con el nombre encontrado y se imprimen sus datos
+        Estudiante encontrado = alumno.Find(m => m.Matricula == z);
+        Escribir("ESTUDIANTE ENCONTRADO: ");
+        Escribir(encontrado.ToString());
+    }
+    else
+        Escribir("ESTUDIANTE NO ENCONTRADO...");
+
+    Escribir("\nPresione cualquier tecla para regresar...");
+    Leer();
+    Console.Clear();
+}
+
+// 6- Desplegar estudinates con promedio > 70 y < a 90
+
+void DesplegarSetentaNoventa(List<Estudiante> alumno)
+{
+    int z = 0;
+    // Utilizamons Where para retornar lista con estudiantes dentro del promedio buscado
+    var setentaNoventa = alumno.Where(entre => entre.Promedio > 70 && entre.Promedio < 90);
+
+    Escribir("Estos son los alumnos con promedio Mayor a 70 y Menor a 90: ");
+
+    foreach(var e in setentaNoventa)
+    {
+        z++;
+        Console.WriteLine(z + ": " + e.ToString() + "\n");  
+    }
+
+    Escribir("\nPresione cualquier tecla para regresar...");
+    Leer();
+    Console.Clear();
+}
+
+// 7- Desplegar estudinates con edades entre 12 y 15
+
+void DesplegarDoceQuince(List<Estudiante> alumno)
+{
+    int z = 0;
+    // Utilizamons Where para retornar lista con estudiantes dentro del rango de edad buscado
+    var doceQuince = alumno.Where(entre => entre.Edad > 12 && entre.Edad < 15);
+
+    Escribir("Estos son los alumnos con edades entre 12 y 15 años: ");
+
+    foreach (var e in doceQuince)
+    {
+        z++;
+        Console.WriteLine(z + ": " + e.ToString() + "\n");
+    }
+
+    Escribir("\nPresione cualquier tecla para regresar...");
+    Leer();
+    Console.Clear();
+}
+
+// 8- Desplegar estudinates por Promedio Descendiente
+
+void DesplegarDescendente(List<Estudiante> alumno)
+{
+    int z = 0;
+    var descendente = alumno.OrderByDescending(e => e.Promedio).ToList();
+
+    Escribir("Estos son los alumnos ordenados por Promedios (Descendente): ");
+
+    foreach (var e in descendente)
+    {
+        z++;
+        Console.WriteLine(z + ": " + e.ToString() + "\n");
+    }
+
+    Escribir("\nPresione cualquier tecla para regresar...");
+    Leer();
+    Console.Clear();
+}
+
+// 9- Desplegar Todos los Estudiantes
+
+void DesplegarEstudiantes(List<Estudiante> alumno)
+{
+    int i = 0;
+    alumno.ForEach(alu => Console.WriteLine(++i + ": " + alu.Nombre));
+
+    Escribir("\nPresione cualquier tecla para regresar...");
     Leer();
     Console.Clear();
 }
@@ -120,8 +262,8 @@ int MostrarMenu()
     Escribir("1- Añadir Estudiante");
     Escribir("2- Editar Estudiante");
     Escribir("3- Eliminar Estudiante");
-    Escribir("4- Buscar Estudiante");
-    Escribir("5- Buscar Estudiante");
+    Escribir("4- Buscar Estudiante (Por Nombre)");
+    Escribir("5- Buscar Estudiante (Por Matricula)");
     Escribir("6- Desplegar Estudiantes con promedio > 70 y < 90");
     Escribir("7- Desplegar Estudiantes que tengan edades comprendidas entre 12 y 15 años");
     Escribir("8- Desplegar Estudiantes por Promedio (Descendente)");
